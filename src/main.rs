1f92f1a10bb1a1032f8c84e4c6185d9d165dc85f
nan mu <mu.nan.11@outlook.com>
2023-12-23 06:56:56 +0800

    现在有了一个很好的输出和一个比较可行的结果。

--- src/main.rs
+++ src/main.rs
@@ -3,28 +3,20 @@
 //! wrench image in /examples by running
 //! `cargo run --example hough ./examples/wrench.jpg ./tmp`
 
-use image::imageops::{resize, thumbnail};
+use image::imageops::thumbnail;
 use image::{open, Luma};
 use imageproc::definitions::{HasBlack, HasWhite};
-use imageproc::edges::canny;
-use imageproc::hough::detect_lines;
-use imageproc::hough::draw_polar_lines;
-use imageproc::hough::LineDetectionOptions;
-use imageproc::hough::PolarLine;
-use imageproc::map::{map_pixels, map_pixels_mut};
+use imageproc::map::map_pixels;
 use std::path::Path;
 
 fn main() {
     let input_path = Path::new("./res/起点.jpg");
-    let output_dir = Path::new("./");
-
+    const sharp: u32 = 20;
     // 插入图片并灰度化 阈值应该为170
     let input_image = open(input_path)
         .expect(&format!("无法加载图像 {:?}", input_path))
         .to_luma8();
-    let input_image = thumbnail(&input_image, 10, 10);
-    let mut flag = 0 as u8;
-    let mut position: Vec<u32> = vec![];
+    let input_image = thumbnail(&input_image, sharp, sharp);
     let threshold_img = map_pixels(&input_image, |x, _, p| {
         if p.0[0] > 170 {
             Luma::<u8>::black()
@@ -37,6 +29,36 @@ fn main() {
             Luma::<u8>::white()
         }
     });
-    let threshold_path = output_dir.join("threshold.jpg");
-    threshold_img.save(&threshold_path).unwrap();
+
+    //left
+    let res = threshold_img.into_vec();
+    for (i, x) in res.iter().enumerate() {
+        print!(
+            "{}",
+            match x {
+                255 => "⬛",
+                _ => "⬜",
+            }
+        );
+        if i as u32 % sharp == sharp - 1 {
+            println!();
+        };
+    }
+    let mut left = 0;
+    for i in 0..sharp / 2 {
+        for row in 0..sharp {
+            if res[(i + row * sharp) as usize] == 255 {
+                left += sharp - row;
+            }
+        }
+    }
+    let mut right = 0;
+    for i in sharp / 2..sharp {
+        for row in 0..sharp {
+            if res[(i + row * sharp) as usize] == 255 {
+                right += sharp - row;
+            }
+        }
+    }
+    println!("{}|{}", left, right);
 }
